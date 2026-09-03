eed9457acce4706121e43eac90e13af6f43d72d9
GUSTAVO MARTINS CASTILHOS <gustavo_castilhos@estudante.sesisenai.org.br>
2026-09-02 21:53:17 -0300

    Feat: add cabecalho

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import './App.css'
+import Cabecalho from './componentes/Cabecalho/Cabecalho'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
 <div>
+  <Cabecalho/>
   <header>Cabeçalho</header>
   <main>Conteudo Principal</main>
   <footer>Rodapé</footer>
---
+++ src/componentes/Cabecalho/Cabecalho.jsx
@@ -0,0 +1,6 @@
+function Cabecalho(){
+return <header>cabecalho...</header>
+
+};
+
+export default Cabecalho;
